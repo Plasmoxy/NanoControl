12221b610066b7860a4d289f619030fd229b58b1
Plasmoxy <sebohacker@gmail.com>
2018-07-28 20:57:26 +0200

    lampen

---
+++ pixelLampen/pixelLampen.ino
@@ -0,0 +1,31 @@
+#include <Adafruit_NeoPixel.h>
+#include "hsvcolors.h"
+
+#define PIN 6
+#define NUMPIXELS 16
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int hueVal = 0;
+int newHueVal = 0;
+float brightnessVal = 0;
+
+void setup() {
+  pixels.begin();
+}
+
+void loop() {
+  newHueVal = map(analogRead(A6), 0, 1024, 0, 360);
+  brightnessVal = analogRead(A7)/1024.0;
+
+  if (newHueVal > hueVal + 6 || newHueVal < hueVal - 6) {
+    hueVal = newHueVal;
+  }
+  
+  int* rgb = hsvToRgb(hueVal, 1.0, brightnessVal);
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(rgb[0], rgb[1], rgb[2]));
+  }
+  pixels.show();
+  delay(50);
+}
